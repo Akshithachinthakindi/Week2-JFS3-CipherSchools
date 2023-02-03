a5ed4b4d344f03a36d38dda2f507817a9f7188f8
Akshitha <chinthakindiakshitha@gmail.com>
2023-02-03 09:55:18 +0530

    updated files

--- SampleProgram.java
+++ SampleProgram.java
@@ -1,13 +1,17 @@
 public class SampleProgram {
     // this keyword
     int arg = 5;
-    void myTest(int arg) {
-        arg=arg;
+    SampleProgram() {
+        System.out.println("Hi ! I am default constructor");
+    }
+    SampleProgram(int arg) {
+        this();
+        this.arg=arg;
     }
     public static void main(String[] args) {
         int arg = 10; // local variable
-        SampleProgram obj = new SampleProgram(); // here we created an object
-        obj.myTest(arg);
+        SampleProgram obj = new SampleProgram(10); // here we created an object
+        //obj.myTest(arg);
         System.out.println(obj.arg);
     }
 }
